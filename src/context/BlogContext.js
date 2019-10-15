61efc4f8244a583711a57f6e3ded025fae12b077
Oscar Garcia <oscarantoniox10@gmail.com>
2019-10-14 21:28:53 -0700

    Commit #2

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -2,10 +2,18 @@ import React from 'react';
 
 const BlogContext = React.createContext();
 
-const BlogProvider = () => {
+export const BlogProvider = ({ children }) => {
+
+    const blogPosts = [
+        {title: 'Blog Post #1'},
+        {title: 'Blog Post #2'}
+    ];
+
     return (
-        <BlogContext>
+        <BlogContext.Provider value={blogPosts} >
             {children}
-        </BlogContext>
+        </BlogContext.Provider>
     )
-}
+};
+
+export default BlogContext;
