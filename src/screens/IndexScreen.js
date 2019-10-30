c04f2a43f7038b161e3ee841f22b729fe3f369c4
Oscar Garcia <oscarantoniox10@gmail.com>
2019-10-29 21:14:46 -0700

    Avance 2

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,23 +1,23 @@
-import React, { useContext } from 'react';
-import { View, Text, StyleSheet, FlatList } from 'react-native';
-import BlogContext from '../context/BlogContext';
+import React, { useContext } from "react";
+import { View, Text, StyleSheet, FlatList, Button } from "react-native";
+import BlogContext from "../context/BlogContext";
 
-const IndexScreen = () =>{
-    const blogPosts = useContext(BlogContext);
+const IndexScreen = () => {
+  const { data, addBlogPost } = useContext(BlogContext);
 
-
-    return(
-        <View>
-            <Text>Index Screen</Text>
-            <FlatList 
-                data={blogPosts} 
-                keyExtractor={(blogPosts)=>blogPosts.title}
-                renderItem={({item}) => {
-                    return <Text> {item.title} </Text>
-                }}
-            />
-        </View>
-    );
+  return (
+    <View>
+      <Text>Index Screen</Text>
+      <Button title="Add Post" onPress={addBlogPost} />
+      <FlatList
+        data={data}
+        keyExtractor={blogPosts => blogPosts.title}
+        renderItem={({ item }) => {
+          return <Text> {item.title} </Text>;
+        }}
+      />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({});
